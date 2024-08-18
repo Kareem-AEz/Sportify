646449b43c38af721b35615b2d9a64c61d04c506
Kareem Ahmed <61951164+Kareem-AEz@users.noreply.github.com>
2024-08-18 11:13:12 +0300

    Add files via upload

--- js/install.js
+++ js/install.js
@@ -31,7 +31,7 @@ window.addEventListener("appinstalled", (evt) => {
 });
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("service-worker.js").then(
+	navigator.serviceWorker.register("js/service-worker.js").then(
 		(registration) => {
 			console.log("Service Worker registered with scope:", registration.scope);
 		},
