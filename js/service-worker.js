046097744c27fa171a636385be9b5139e0342b98
Kareem Ahmed <61951164+Kareem-AEz@users.noreply.github.com>
2024-08-18 12:25:57 +0300

    Add files via upload

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,26 +1,12 @@
-self.addEventListener("install", (event) => {
-	// Skip waiting so that the new service worker activates immediately
-	self.skipWaiting();
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
 });
 
-self.addEventListener("activate", (event) => {
-	event.waitUntil(
-		caches.keys().then((cacheNames) => {
-			return Promise.all(
-				cacheNames.map((cacheName) => {
-					return caches.delete(cacheName);
-				})
-			);
-		})
-	);
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating.');
 });
 
-// Fetch event and other service worker logic
-self.addEventListener("fetch", (event) => {
-	// Example fetch handler
-	event.respondWith(
-		caches.match(event.request).then((response) => {
-			return response || fetch(event.request);
-		})
-	);
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
 });
+
